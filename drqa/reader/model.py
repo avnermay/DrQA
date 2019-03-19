de47fa69f8d5e6456f06058a99bf33711ef7d300
zjian <zjian@stanford.edu>
2019-03-19 01:00:04 -0700

    make adamax has tunable learning ratefor drqa

--- drqa/reader/model.py
+++ drqa/reader/model.py
@@ -182,7 +182,7 @@ class DocReader(object):
                                        momentum=self.args.momentum,
                                        weight_decay=self.args.weight_decay)
         elif self.args.optimizer == 'adamax':
-            self.optimizer = optim.Adamax(parameters, lr=args.learning_rate,
+            self.optimizer = optim.Adamax(parameters, lr=self.args.learning_rate,
                                           weight_decay=self.args.weight_decay)
         else:
             raise RuntimeError('Unsupported optimizer: %s' %
